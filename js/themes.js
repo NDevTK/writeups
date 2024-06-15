7370663b703828a7322fce622aed227d452a2cc8
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-06-16 00:23:29 +0100

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,9 +1,16 @@
   "use strict";
   const theme = localStorage.getItem('theme') || 'default';
-  
+
+  // default is handled without JS
   if (theme !== 'default') {
     const stylesheet = document.createElement('link');
     stylesheet.href = '/writeups/themes/' + encodeURIComponent(theme);
     stylesheet.rel = 'stylesheet';
     document.head.appendChild(stylesheet);
+  }
+
+  const reload = new BroadcastChannel('reload');
+  reload.onmessage = (event) => {
+    if (location.origin === event.origin) return;
+    location.reload();
   }
