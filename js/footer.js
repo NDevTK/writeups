8ce0b012cd402adee4a35ff3aaa8141c8f0fc4e6
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-06-28 05:31:17 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -30,10 +30,3 @@
   const translate = document.getElementById('translate');
   // Dont translate the google translate
   if (location.origin.endsWith('.translate.goog')) translate.hidden = true;
-
-  // Auto reload the page every 30 minutes (Keep content up to date)
-  // This should keep the scroll position and only run when hidden
-  const autoReload = 1000 * 60 * 30;
-  setInterval(() => {
-    if (document.visibilityState === 'hidden') location.reload();
-  }, autoReload);
