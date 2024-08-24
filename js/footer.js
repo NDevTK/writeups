7569c1f000567bdaaa32fbf36361eeaf9c9a2c97
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-08-24 14:22:42 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -18,10 +18,6 @@
   // Dont assume the user has javascript enabled and no clickjacking.
   if (window.top == window) themes.disabled = false;
 
-  const translate = document.getElementById('translate');
-  // Dont translate the google translate
-  if (location.origin.endsWith('.translate.goog')) translate.hidden = true;
-
  if (theme === 'base64.css') {
   [...document.body.querySelectorAll('p, a')].forEach(e => { e.innerText = btoa(encodeURI(e.innerText)) });
  }
