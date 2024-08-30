bc9bada0cfab0640908caab7324097195c48239f
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-08-30 13:05:41 +0100

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,31 +1,30 @@
   "use strict";
 
-	function getTheme() {
-		try {
-			return localStorage.getItem('theme') || 'default';
-		} catch {
-			return 'default';
-		}
-	}
+  function getTheme() {
+      try {
+          return localStorage.getItem('theme') || 'default';
+      } catch {
+          return 'default';
+      }
+  }
 
   const theme = getTheme();
 
   // default is handled without JS
   if (theme !== 'default') {
-    const stylesheet = document.createElement('link');
-    stylesheet.href = '/writeups/themes/' + encodeURIComponent(theme);
-    stylesheet.rel = 'stylesheet';
-    document.head.appendChild(stylesheet);
+      const stylesheet = document.createElement('link');
+      stylesheet.href = '/writeups/themes/' + encodeURIComponent(theme);
+      stylesheet.rel = 'stylesheet';
+      document.head.appendChild(stylesheet);
   }
 
   const reload = new BroadcastChannel('reload');
   reload.onmessage = (event) => {
-    if (location.origin !== event.origin) return;
-    location.reload();
-  }
-  
-  onkeyup = (e) => {
-    if (e.key === 'ArrowLeft' ) history.back();
-    if (e.key === 'ArrowRight' ) history.forward();
+      if (location.origin !== event.origin) return;
+      location.reload();
   }
 
+  onkeyup = (e) => {
+      if (e.key === 'ArrowLeft') history.back();
+      if (e.key === 'ArrowRight') history.forward();
+  }
