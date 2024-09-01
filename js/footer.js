79047879e352d50af6fd43d394addb5c9aaf8000
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-09-01 04:11:10 +0100

    filter random

--- js/footer.js
+++ js/footer.js
@@ -14,8 +14,9 @@ themes.onchange = () => {
     localStorage.removeItem('theme');
   } else {
     if (themes.value == 'random') {
+      const allowedThemes = [...themes.options].filter((e) => { return e.value != 'random' && e.value != theme});
       themes.value =
-        themes.options[Math.floor(Math.random() * themes.options.length)].value;
+        allowedThemes[Math.floor(Math.random() * themes.options.length)].value;
     }
     if (themes.value == 'mc.css') {
       // https://www.minecraft.net/en-us/usage-guidelines
