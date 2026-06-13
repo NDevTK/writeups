96d5047b2a2729e5a77462304971753d041aaa4f
NDevTK <31563761+NDevTK@users.noreply.github.com>
2026-06-13 14:31:53 +0100

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -36,7 +36,7 @@ if (theme.endsWith('.html')) {
     'margin:0',
     'z-index:-1'
   ].join(';');
-  document.body.prepend(iframe);
+  document.body.prepend(frame);
 
   var btn = document.createElement('button');
   btn.textContent = '✦';
