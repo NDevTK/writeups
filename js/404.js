620a5107475959914bf40f90935b16d3d37a4027
NDevTK <31563761+NDevTK@users.noreply.github.com>
2026-06-13 21:02:31 +0100

    Add 404.js for theme redirection handling

---
+++ js/404.js
@@ -0,0 +1,6 @@
+if (location.pathname.startsWith('/writeups/themes/')) {
+  document.body.innerText = 'about:blank#themenotfound';
+} else {
+  let suffix = (theme == 'default.css') ? '' : '?theme=' + encodeURIComponent(theme);
+  location.href = 'https://ndevtk.github.io/writeups/' + suffix;
+}
